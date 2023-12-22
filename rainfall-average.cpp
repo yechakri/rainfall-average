fe8e110f45b08b129a7bfdd0a91b95fa4af44ec0
yechakri <154390370+yechakri@users.noreply.github.com>
2023-12-21 22:50:24 -0600

    Update rainfall-average.cpp
    Added variables and a block that prompts the user to enter data.

--- rainfall-average.cpp
+++ rainfall-average.cpp
@@ -7,6 +7,28 @@ using namespace std;
 
 int main()
 {
+      // Variables 
+    string month1, month2, month3;    
+    double rain1, rain2, rain3, average;   
+
+    cout << "Please enter the name of the first month: ";
+    getline(cin, month1);
+    cout << "The amount of rain that fell in " << month1 << " (in inches): ";
+    cin >> rain1;
+    cin.ignore();
+
+    cout << "The name of the second month: ";
+    getline(cin, month2);
+    cout << "The amount of rain that fell in " << month2 << " (in inches): ";
+    cin >> rain2;
+    cin.ignore();
+
+    cout << "The name of the third month: ";
+    getline(cin, month3);
+    cout << "The amount of rain that fell in " << month3 << " (in inches): ";
+    cin >> rain3;
+    cin.ignore();
+
 
     return 0;
 }
