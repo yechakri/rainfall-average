74d71f2c8b1d1762c0f26effbdd9b9aaf160f5ae
yechakri <154390370+yechakri@users.noreply.github.com>
2023-12-21 22:46:54 -0600

    Create rainfall-average.cpp
    Created the rainfall-average.cpp file

---
+++ rainfall-average.cpp
@@ -0,0 +1,12 @@
+// This program calculates the average rainfall for three months
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
